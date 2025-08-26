0e0cabf0920f3b909a833795914d4ccc0cf17b55
TurboVazbudatel <edit.openai@gmail.com>
2025-08-26 18:21:48 +0600

    test: use hive_test (in-memory) + proper setup/teardown to avoid hanging

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,24 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:moneyquest_quest/main.dart';
+import 'package:moneyquest_quest/data/storage/hive_store.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_test/hive_test.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUp(() async {
+    await setUpTestHive();                   // in-memory Hive
+    await Hive.openBox(HiveStore.txBox);     // открываем нужный бокс
+  });
+
+  tearDown(() async {
+    await Hive.box(HiveStore.txBox).close(); // закрываем
+    await tearDownTestHive();                // чистим память
+  });
+
   testWidgets('MoneyQuest smoke test', (tester) async {
     await tester.pumpWidget(const MoneyQuestApp());
-    // Проверим, что корневой виджет отрисовался
     expect(find.byType(MoneyQuestApp), findsOneWidget);
   });
 }
