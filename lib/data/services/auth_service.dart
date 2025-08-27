30ce98cb1314676e41b753a06f4fd5b9567b11cb
TurboVazbudatel <edit.openai@gmail.com>
2025-08-27 12:52:35 +0600

    fix(auth): add signOut() to AuthService used by Account screen

--- lib/data/services/auth_service.dart
+++ lib/data/services/auth_service.dart
@@ -1,30 +1,17 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AuthService {
-  final _auth = FirebaseAuth.instance;
+  final _fa = FirebaseAuth.instance;
 
-  Stream<User?> get onAuthStateChanged => _auth.authStateChanges();
-  User? get currentUser => _auth.currentUser;
-
-  Future<UserCredential> signInAnonymously() async {
-    return await _auth.signInAnonymously();
-  }
-
-  Future<UserCredential> signInWithApple() async {
-    final credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [AppleIDAuthorizationScopes.email, AppleIDAuthorizationScopes.fullName],
-    );
-
-    final oauth = OAuthProvider("apple.com").credential(
-      idToken: credential.identityToken,
-      accessToken: credential.authorizationCode,
-    );
-
-    return await _auth.signInWithCredential(oauth);
+  Future<User?> signInAnonymously() async {
+    final cred = await _fa.signInAnonymously();
+    return cred.user;
   }
 
   Future<void> signOut() async {
-    await _auth.signOut();
+    await _fa.signOut();
   }
+
+  User? get currentUser => _fa.currentUser;
+  bool get isLoggedIn => _fa.currentUser != null;
 }
